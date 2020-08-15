2ab98b7f8ca601f01d8815b6b801bcfdf01ee02e
artem.morozov <artembo@me.com>
2020-08-16 01:52:04 +0300

    added dbapi-compliance to appveyor dependencies

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 set -exu # Strict shell (w/o -o pipefail)
 
 # Install tarantool.
-curl -L https://tarantool.io/installer.sh | VER=2.4 bash
+curl -L https://tarantool.io/installer.sh | VER=2.4 sudo -E bash
 
 # Install testing dependencies.
 pip install -r requirements.txt
